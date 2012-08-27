c849425f0555467f662d6c525a60f4ef666a539b
Gurpreet Nanda <gnanda@mines.edu>
2012-08-26 22:28:07 -0600

    Rewrote first project to update time displayed on a button when it is pressed

--- HelloAndroid/src/csci498/hello/HelloActivity.java
+++ HelloAndroid/src/csci498/hello/HelloActivity.java
@@ -1,13 +1,32 @@
 package csci498.hello;
 
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HelloActivity extends Activity {
+public class HelloActivity extends Activity implements View.OnClickListener {
+	Button btn;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        btn = new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
     }
+
+	@Override
+	public void onClick(View arg0) {
+		updateTime();
+	}
+    
+	private void updateTime() {
+		btn.setText(new Date().toString());
+	}
+    
 }
