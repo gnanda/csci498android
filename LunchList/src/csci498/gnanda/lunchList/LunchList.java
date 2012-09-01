0ba2030aed7c541d7c08090f71a67d99cfec39f6
Gurpreet Nanda <gnanda@mines.edu>
2012-08-31 18:44:10 -0600

    Implemented toString for Restaurant class

--- LunchList/src/csci498/gnanda/lunchList/LunchList.java
+++ LunchList/src/csci498/gnanda/lunchList/LunchList.java
@@ -1,14 +1,18 @@
 package csci498.gnanda.lunchList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class LunchList extends Activity {
 	
-	private Restaurant r = new Restaurant();
+	private List<Restaurant> model = new ArrayList<Restaurant>();
 	
     /** Called when the activity is first created. */
     @Override
@@ -17,20 +21,41 @@ public class LunchList extends Activity {
         setContentView(R.layout.main);
         
         Button save = (Button) findViewById(R.id.save);
-        save.setOnClickListener(onSave);     
+        save.setOnClickListener(onSave);          
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
+			Restaurant r = new Restaurant();
+			
 			EditText name = (EditText) findViewById(R.id.name);
 			EditText address = (EditText) findViewById(R.id.addr);
 			
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
+			
+			addRadioGroupType(r);
 		}
 		
 	};
-    
+
+	private void addRadioGroupType(Restaurant r) {
+	    RadioGroup types = (RadioGroup) findViewById(R.id.types);
+	    
+	    switch (types.getCheckedRadioButtonId()) {
+	      case R.id.sit_down:
+	        r.setType("sit_down");
+	        break;
+	        
+	      case R.id.take_out:
+	        r.setType("take_out");
+	        break;
+	        
+	      case R.id.delivery:
+	        r.setType("delivery");
+	        break;
+	    }
+	}    
 }
--- LunchList/src/csci498/gnanda/lunchList/Restaurant.java
+++ LunchList/src/csci498/gnanda/lunchList/Restaurant.java
@@ -4,6 +4,7 @@ public class Restaurant {
 	
 	private String name = "";
 	private String address = "";
+	private String type = "";
 	
 	public String getName() {
 		return name;
@@ -20,5 +21,17 @@ public class Restaurant {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
 }
