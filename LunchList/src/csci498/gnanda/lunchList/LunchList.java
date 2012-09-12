22531c55a9b901a1601635736fb8105e3968e1fc
Gurpreet Nanda <gnanda@mines.edu>
2012-09-11 21:56:32 -0600

    Added a long task

--- LunchList/src/csci498/gnanda/lunchList/LunchList.java
+++ LunchList/src/csci498/gnanda/lunchList/LunchList.java
@@ -42,6 +42,14 @@ public class LunchList extends TabActivity {
 	private List<String> addresses = new ArrayList<String>();
 	private ArrayAdapter<String> addressesAdapter = null;
 	
+	private Runnable longTask = new Runnable() {
+		public void run() {
+			for (int i = 0; i < 20; i++) {
+				doSomeLongWork(500);
+			}
+		}
+	};
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
