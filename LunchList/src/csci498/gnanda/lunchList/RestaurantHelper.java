f0e3e1542c04d3129a82f50836a7ba9a5fc4984a
Gurpreet Nanda <gnanda@mines.edu>
2012-09-18 21:16:38 -0600

    Created RestaurantHelper class for database access

---
+++ LunchList/src/csci498/gnanda/lunchList/RestaurantHelper.java
@@ -0,0 +1,26 @@
+package csci498.gnanda.lunchList;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class RestaurantHelper extends SQLiteOpenHelper {
+	
+	private static final String DATABASE_NAME = "lunchlist.db";
+	public static final int SCHEMA_VERSION = 1;
+	
+	public RestaurantHelper(Context context) {
+		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+	}
+
+}
