39895ce5f6d26922f5cbc7c1a53052e22cde6986
Gurpreet Nanda <gnanda@mines.edu>
2012-09-24 01:50:08 -0600

    Added 'add' option menu

--- LunchList/src/csci498/gnanda/lunchList/LunchList.java
+++ LunchList/src/csci498/gnanda/lunchList/LunchList.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -51,6 +54,21 @@ public class LunchList extends ListActivity {
 		address.setAdapter(addressesAdapter); 
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.option, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.add) {
+			startActivity(new Intent(LunchList.this, DetailForm.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();		
