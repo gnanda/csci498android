8a94ac134eebb0f02b3639eb375d2c310e87f380
Gurpreet Nanda <gnanda@mines.edu>
2012-10-08 20:13:38 -0600

    Added stub for FeedService class

---
+++ LunchList/src/csci498/gnanda/lunchList/FeedService.java
@@ -0,0 +1,18 @@
+package csci498.gnanda.lunchList;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+
+	public FeedService() {
+		super("FeedService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
