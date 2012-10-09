1121ec632d39206a461732da33bcb4047fde788e
Gurpreet Nanda <gnanda@mines.edu>
2012-10-08 20:18:00 -0600

    Implemented rssfeed loading in the onHandleIntent method

--- LunchList/src/csci498/gnanda/lunchList/FeedService.java
+++ LunchList/src/csci498/gnanda/lunchList/FeedService.java
@@ -1,9 +1,15 @@
 package csci498.gnanda.lunchList;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	
+	private static final String EXTRA_URL = "csci498.gnanda.lunchlist.EXTRA_URL";
 
 	public FeedService() {
 		super("FeedService");
@@ -11,8 +17,13 @@ public class FeedService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
+		RSSReader reader = new RSSReader();
 		
+		try {
+			RSSFeed result = reader.load(intent.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 	
 }
