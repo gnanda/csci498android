b3e7e14d6296009d5bf6171eb8baebd3af7cbb0a
Gurpreet Nanda <gnanda@mines.edu>
2012-10-08 20:22:52 -0600

    Added messenger and message to FeedService

--- LunchList/src/csci498/gnanda/lunchList/FeedService.java
+++ LunchList/src/csci498/gnanda/lunchList/FeedService.java
@@ -3,13 +3,17 @@ package csci498.gnanda.lunchList;
 import org.mcsoxford.rss.RSSFeed;
 import org.mcsoxford.rss.RSSReader;
 
+import android.app.Activity;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 
 public class FeedService extends IntentService {
 	
 	private static final String EXTRA_URL = "csci498.gnanda.lunchlist.EXTRA_URL";
+	private static final String EXTRA_MESSENGER = "csci498.gnadna.lunchlist.EXTRA_MESSENGER";
 
 	public FeedService() {
 		super("FeedService");
@@ -18,11 +22,24 @@ public class FeedService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent intent) {
 		RSSReader reader = new RSSReader();
+		Messenger messenger = (Messenger) intent.getExtras().get(EXTRA_MESSENGER);
+		Message msg = Message.obtain();
 		
 		try {
 			RSSFeed result = reader.load(intent.getStringExtra(EXTRA_URL));
+			
+			msg.arg1 = Activity.RESULT_OK;
+			msg.obj = result;
 		} catch (Exception e) {
 			Log.e("LunchList", "Exception parsing feed", e);
+			msg.arg1 = Activity.RESULT_CANCELED;
+			msg.obj = e;
+		}
+		
+		try {
+			messenger.send(msg);
+		} catch (Exception e) {
+			Log.w("LunchList", "Exception sending results to activity", e);
 		}
 	}
 	
