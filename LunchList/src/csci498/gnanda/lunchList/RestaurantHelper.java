1d8c3c687d167c35ce65f6e5e5ed662f54b3f42b
Gurpreet Nanda <gnanda@mines.edu>
2012-10-18 20:40:15 -0600

    Added latitude and longitude to the data model

--- LunchList/src/csci498/gnanda/lunchList/RestaurantHelper.java
+++ LunchList/src/csci498/gnanda/lunchList/RestaurantHelper.java
@@ -9,7 +9,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class RestaurantHelper extends SQLiteOpenHelper {
 	
 	private static final String DATABASE_NAME = "lunchlist.db";
-	public static final int SCHEMA_VERSION = 2;
+	private static final String CREATE_TABLE_SQL = "CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);";
+	private static final String SELECT_BY_ID_SQL = "SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?";
+	private static final String SELECT_ALL_SQL = "SELECT _id, name, address, type, notes, feed, lat lon FROM restaurants ORDER BY ";
+	public static final int SCHEMA_VERSION = 3;
 	
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -17,17 +20,24 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT);");
+		db.execSQL(CREATE_TABLE_SQL);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		if (oldVersion < 2) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		}
+		
+		if (oldVersion < 3) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lon REAL");
+		}		
 	}
 	
 	public Cursor getById(String id) {
 		String[] args = {id};
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants WHERE _ID=?", args);
+		return getReadableDatabase().rawQuery(SELECT_BY_ID_SQL, args);
 	}
 	
 	public void update(String id, String name, String address, String type, String notes, String feed) {
@@ -52,9 +62,19 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		
 		getWritableDatabase().insert("Restaurants", "name", cv);
 	}
+	
+	public void updateLocation(String id, double lat, double lon) {
+		ContentValues cv = new ContentValues();
+		String[] args = {id};
+		
+		cv.put("lat", lat);
+		cv.put("lon", lon);
+		
+		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
+	}
 
 	public Cursor getAll(String orderBy) {
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants ORDER BY " + orderBy, null);
+		return getReadableDatabase().rawQuery(SELECT_ALL_SQL + orderBy, null);
 	}
 	
 	public String getName(Cursor c) {
@@ -75,6 +95,14 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 
 	public String getFeed(Cursor c) {
 		return c.getString(5);
+	}
+	
+	public String getLatitude(Cursor c) {
+		return c.getString(6);
+	}
+	
+	public String getLongitude(Cursor c) {
+		return c.getString(7);
 	}
 	
 }
