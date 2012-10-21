3575681b8da9821c85ba4a5edefbb2dd3395b7c9
Gurpreet Nanda <gnanda@mines.edu>
2012-10-21 17:09:48 -0600

    Set the zoom degree and center of the map to show the location passed in

--- LunchList/src/csci498/gnanda/lunchList/RestaurantMap.java
+++ LunchList/src/csci498/gnanda/lunchList/RestaurantMap.java
@@ -2,18 +2,32 @@ package csci498.gnanda.lunchList;
 
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class RestaurantMap extends MapActivity {
 	
 	public static final String EXTRA_LATITUDE = "csci498.gnanda.lunchList.EXTRA_LATITUDE";
 	public static final String EXTRA_LONGITUDE = "csci498.gnanda.lunchList.EXTRA_LONGITUDE";
 	public static final String EXTRA_NAME = "csci498.gnanda.lunchList.EXTRA_NAME";
+	private MapView map;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
+
+		double lat = getIntent().getDoubleExtra(EXTRA_LATITUDE, 0);
+		double lon = getIntent().getDoubleExtra(EXTRA_LONGITUDE, 0);
+		
+		map = (MapView) findViewById(R.id.map);
+		map.getController().setZoom(17);
+		
+		GeoPoint status = new GeoPoint((int) (lat * 1000000.0), (int) (lon * 1000000.0));
+		
+		map.getController().setCenter(status);
+		map.setBuiltInZoomControls(true);
 	}
 
 	@Override
