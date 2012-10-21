15b52fffb8f7f5073f0b919be5c7798203da577d
Gurpreet Nanda <gnanda@mines.edu>
2012-10-21 16:48:44 -0600

    Added options menu for viewing map

---
+++ LunchList/src/csci498/gnanda/lunchList/RestaurantMap.java
@@ -0,0 +1,13 @@
+package csci498.gnanda.lunchList;
+
+import com.google.android.maps.MapActivity;
+
+public class RestaurantMap extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
