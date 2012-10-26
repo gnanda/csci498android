7407ee3cbab33eebce5f59eb923a23e527eb2db7
Gurpreet Nanda <gnanda@mines.edu>
2012-10-26 14:16:17 -0600

    Added OnBootReceiver class, and added to manifest

---
+++ LunchList/src/csci498/gnanda/lunchList/OnBootReceiver.java
@@ -0,0 +1,14 @@
+package csci498.gnanda.lunchList;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context ctxt, Intent intent) {
+		// Do Something
+	}
+
+}
