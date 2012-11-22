ea6ebec7176dc0344830bda71369aa1b86420ed7
Gurpreet Nanda <gnanda@mines.edu>
2012-11-21 18:00:06 -0700

    Changed RestaurantHelper to account for the addition of phone numbers

--- LunchList/src/csci498/gnanda/lunchList/RestaurantHelper.java
+++ LunchList/src/csci498/gnanda/lunchList/RestaurantHelper.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class RestaurantHelper extends SQLiteOpenHelper {
 	
+	private static final String PHONE2 = "phone";
 	private static final String FEED2 = "feed";
 	private static final String NOTES2 = "notes";
 	private static final String TYPE2 = "type";
@@ -16,11 +17,12 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	private static final String ADD_COLUMN_LON = "ALTER TABLE restaurants ADD COLUMN lon REAL";
 	private static final String ADD_COLUMN_LAT = "ALTER TABLE restaurants ADD COLUMN lat REAL";
 	private static final String ADD_COLUMN_FEED = "ALTER TABLE restaurants ADD COLUMN feed TEXT";
+	private static final String ADD_COLUMN_PHONE = "ALTER TABLE restaurants ADD COLUMN phone TEXT";
 	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final String CREATE_TABLE_SQL = "CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);";
-	private static final String SELECT_BY_ID_SQL = "SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?";
-	private static final String SELECT_ALL_SQL = "SELECT _id, name, address, type, notes, feed, lat lon FROM restaurants ORDER BY ";
-	public static final int SCHEMA_VERSION = 3;
+	private static final String CREATE_TABLE_SQL = "CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL, phone TEXT);";
+	private static final String SELECT_BY_ID_SQL = "SELECT _id, name, address, type, notes, feed, lat, lon, phone FROM restaurants WHERE _ID=?";
+	private static final String SELECT_ALL_SQL = "SELECT _id, name, address, type, notes, feed, lat, phone lon FROM restaurants ORDER BY ";
+	public static final int SCHEMA_VERSION = 4;
 	
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -41,6 +43,10 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 			db.execSQL(ADD_COLUMN_LAT);
 			db.execSQL(ADD_COLUMN_LON);
 		}		
+		
+		if (oldVersion < 4) {
+			db.execSQL(ADD_COLUMN_PHONE);
+		}
 	}
 	
 	public Cursor getById(String id) {
@@ -48,7 +54,7 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		return getReadableDatabase().rawQuery(SELECT_BY_ID_SQL, args);
 	}
 	
-	public void update(String id, String name, String address, String type, String notes, String feed) {
+	public void update(String id, String name, String address, String type, String notes, String feed, String phone) {
 		ContentValues cv = new ContentValues();
 		String[] args = {id};
 		cv.put(NAME2, name);
@@ -56,17 +62,19 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put(TYPE2, type);
 		cv.put(NOTES2, notes);
 		cv.put(FEED2, feed);
+		cv.put(PHONE2, phone);
 		
 		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
 	
-	public void insert(String name, String address, String type, String notes, String feed) {
+	public void insert(String name, String address, String type, String notes, String feed, String phone) {
 		ContentValues cv = new ContentValues();
 		cv.put(NAME2, name);
 		cv.put(ADDRESS2, address);
 		cv.put(TYPE2, type);
 		cv.put(NOTES2, notes);
 		cv.put(FEED2, feed);
+		cv.put(PHONE2, phone);
 		
 		getWritableDatabase().insert("Restaurants", NAME2, cv);
 	}
@@ -111,6 +119,10 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	
 	public double getLongitude(Cursor c) {
 		return c.getDouble(7);
+	}
+	
+	public String getPhone(Cursor c) {
+		return c.getString(8);
 	}
 	
 }
